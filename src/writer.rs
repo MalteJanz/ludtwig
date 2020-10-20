a9620ce3723806734c73e1c43a2d947dede21337
Malte Janz <malte.janz@yahoo.de>
2020-10-20 09:43:56 +0200

    Implemented indentation and attribute printing

--- src/writer.rs
+++ src/writer.rs
@@ -21,47 +21,67 @@ pub async fn write_tree(path: PathBuf, tree: &HtmlNode) {
     let file = File::create(path).await.expect("can't create file.");
     let mut writer = BufWriter::new(file);
 
-    print_node(&mut writer, tree).await;
+    print_node(&mut writer, tree, 0).await;
 
     writer.flush().await.unwrap();
 }
 
-// ToDo accept indentation and apply it.
 fn print_node<'a>(
     writer: &'a mut BufWriter<File>,
     node: &'a HtmlNode,
+    indentation: u16,
 ) -> Pin<Box<dyn Future<Output = ()> + 'a>> {
     Box::pin(async move {
         match node {
             HtmlNode::Root(root) => {
                 for child in root {
-                    print_node(writer, child).await;
+                    print_node(writer, child, indentation).await;
                 }
             }
             HtmlNode::Tag(tag) => {
-                print_tag(writer, &tag).await;
+                print_tag(writer, &tag, indentation).await;
             }
             HtmlNode::Plain(plain) => {
-                print_plain(writer, &plain).await;
+                print_plain(writer, &plain, indentation).await;
             }
             HtmlNode::VueBlock(vue) => {
-                print_vue_block(writer, &vue).await;
+                print_vue_block(writer, &vue, indentation).await;
             }
             HtmlNode::TwigBlock(twig) => {
-                print_twig_block(writer, &twig).await;
+                print_twig_block(writer, &twig, indentation).await;
             }
             HtmlNode::TwigParentCall => {
-                print_twig_parent_call(writer).await;
+                print_twig_parent_call(writer, indentation).await;
             }
         }
     })
 }
 
-async fn print_tag(writer: &mut BufWriter<File>, tag: &HtmlTag) {
+async fn print_tag(writer: &mut BufWriter<File>, tag: &HtmlTag, indentation: u16) {
+    print_indentation(writer, indentation).await;
+
     writer.write_all("<".as_bytes()).await.unwrap();
     writer.write_all(tag.name.as_bytes()).await.unwrap();
 
-    // ToDo attributes
+    // attributes
+    for (key, value) in &tag.attributes {
+        if tag.attributes.len() > 2 {
+            writer.write_all("\n".as_bytes()).await.unwrap();
+            print_indentation(writer, indentation + 2).await;
+        } else {
+            writer.write_all(" ".as_bytes()).await.unwrap();
+        }
+
+        writer.write_all(key.as_bytes()).await.unwrap();
+
+        if value == "" {
+            continue;
+        }
+
+        writer.write_all("=\"".as_bytes()).await.unwrap();
+        writer.write_all(value.as_bytes()).await.unwrap();
+        writer.write_all("\"".as_bytes()).await.unwrap();
+    }
 
     if tag.self_closed {
         writer.write_all("/>\n".as_bytes()).await.unwrap();
@@ -70,9 +90,10 @@ async fn print_tag(writer: &mut BufWriter<File>, tag: &HtmlTag) {
     }
 
     for child in &tag.children {
-        print_node(writer, child).await;
+        print_node(writer, child, indentation + 1).await;
     }
 
+    print_indentation(writer, indentation).await;
     if !tag.self_closed {
         writer.write_all("</".as_bytes()).await.unwrap();
         writer.write_all(tag.name.as_bytes()).await.unwrap();
@@ -80,32 +101,43 @@ async fn print_tag(writer: &mut BufWriter<File>, tag: &HtmlTag) {
     }
 }
 
-async fn print_plain(writer: &mut BufWriter<File>, plain: &HtmlPlain) {
+async fn print_plain(writer: &mut BufWriter<File>, plain: &HtmlPlain, indentation: u16) {
+    print_indentation(writer, indentation).await;
     writer.write_all(plain.plain.as_bytes()).await.unwrap();
     writer.write_all("\n".as_bytes()).await.unwrap();
 }
 
-async fn print_vue_block(writer: &mut BufWriter<File>, vue: &VueBlock) {
+async fn print_vue_block(writer: &mut BufWriter<File>, vue: &VueBlock, indentation: u16) {
+    print_indentation(writer, indentation).await;
     writer.write_all("{{ ".as_bytes()).await.unwrap();
     writer.write_all(vue.content.as_bytes()).await.unwrap();
     writer.write_all(" }}\n".as_bytes()).await.unwrap();
 }
 
-async fn print_twig_block(writer: &mut BufWriter<File>, twig: &TwigBlock) {
+async fn print_twig_block(writer: &mut BufWriter<File>, twig: &TwigBlock, indentation: u16) {
+    print_indentation(writer, indentation).await;
     writer.write_all("{% block ".as_bytes()).await.unwrap();
     writer.write_all(twig.name.as_bytes()).await.unwrap();
     writer.write_all(" %}\n".as_bytes()).await.unwrap();
 
     for child in &twig.children {
-        print_node(writer, child).await;
+        print_node(writer, child, indentation + 1).await;
     }
 
+    print_indentation(writer, indentation).await;
     writer
         .write_all("{% endblock %}\n".as_bytes())
         .await
         .unwrap();
 }
 
-async fn print_twig_parent_call(writer: &mut BufWriter<File>) {
+async fn print_twig_parent_call(writer: &mut BufWriter<File>, indentation: u16) {
+    print_indentation(writer, indentation).await;
     writer.write_all("{% parent %}\n".as_bytes()).await.unwrap();
+}
+
+async fn print_indentation(writer: &mut BufWriter<File>, indentation: u16) {
+    for _ in 0..indentation {
+        writer.write_all("    ".as_bytes()).await.unwrap();
+    }
 }
