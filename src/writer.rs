6efebad57df968a78685352bac63eb239ef50072
Malte Janz <malte.janz@yahoo.de>
2020-10-21 22:45:08 +0200

    Added context to printing with previous, after and all parent nodes as references

--- src/writer.rs
+++ src/writer.rs
@@ -5,6 +5,38 @@ use tokio::fs::File;
 use tokio::io::{AsyncWriteExt, BufWriter};
 use twig::ast::{HtmlNode, HtmlPlain, HtmlTag, TwigBlock, VueBlock};
 
+#[derive(Clone, PartialEq)]
+struct PrintingContext<'a> {
+    previous_node: Option<&'a HtmlNode>,
+    after_node: Option<&'a HtmlNode>,
+
+    /// the last node in the list is the current node. everything before that is up in the hierarchy.
+    parent_nodes: Vec<&'a HtmlNode>,
+
+    /// in tab count
+    indentation: u16,
+}
+
+impl<'a> PrintingContext<'a> {
+    /// clones the current context and returns a new one with the increased indentation.
+    fn increase_indentation_by(&self, increase: u16) -> Self {
+        let mut copy = self.clone();
+        copy.indentation += increase;
+        copy
+    }
+}
+
+impl<'a> Default for PrintingContext<'a> {
+    fn default() -> Self {
+        PrintingContext {
+            previous_node: None,
+            after_node: None,
+            parent_nodes: vec![],
+            indentation: 0,
+        }
+    }
+}
+
 pub async fn write_tree(path: PathBuf, tree: &HtmlNode) {
     // ToDo: replace this after done with testing.
     let base_path = Path::new("output");
@@ -18,7 +50,7 @@ pub async fn write_tree(path: PathBuf, tree: &HtmlNode) {
     let file = File::create(path).await.expect("can't create file.");
     let mut writer = BufWriter::new(file);
 
-    print_node(&mut writer, tree, 0).await;
+    print_node(&mut writer, tree, &mut PrintingContext::default()).await;
 
     writer.flush().await.unwrap();
 }
@@ -26,43 +58,60 @@ pub async fn write_tree(path: PathBuf, tree: &HtmlNode) {
 fn print_node<'a>(
     writer: &'a mut BufWriter<File>,
     node: &'a HtmlNode,
-    indentation: u16,
+    context: &'a mut PrintingContext<'a>,
 ) -> Pin<Box<dyn Future<Output = ()> + 'a>> {
     Box::pin(async move {
+        context.parent_nodes.push(&node);
+
         match node {
             HtmlNode::Root(root) => {
-                print_node_list(writer, &root, indentation).await;
+                print_node_list(writer, &root, context).await;
             }
             HtmlNode::Tag(tag) => {
-                print_tag(writer, &tag, indentation).await;
+                print_tag(writer, &tag, context).await;
             }
             HtmlNode::Plain(plain) => {
-                print_plain(writer, &plain, indentation).await;
+                print_plain(writer, &plain, context).await;
             }
             HtmlNode::VueBlock(vue) => {
-                print_vue_block(writer, &vue, indentation).await;
+                print_vue_block(writer, &vue, context).await;
             }
             HtmlNode::TwigBlock(twig) => {
-                print_twig_block(writer, &twig, indentation).await;
+                print_twig_block(writer, &twig, context).await;
             }
             HtmlNode::TwigParentCall => {
-                print_twig_parent_call(writer, indentation).await;
+                print_twig_parent_call(writer, context).await;
             }
             HtmlNode::Whitespace => {
-                print_whitespace(writer).await;
+                print_whitespace(writer, context).await;
             }
         }
     })
 }
 
-async fn print_node_list(writer: &mut BufWriter<File>, nodes: &Vec<HtmlNode>, indentation: u16) {
-    for node in nodes {
-        print_node(writer, node, indentation).await;
+async fn print_node_list(
+    writer: &mut BufWriter<File>,
+    nodes: &Vec<HtmlNode>,
+    context: &PrintingContext<'_>,
+) {
+    for idx in 0..nodes.len() {
+        let previous = if idx > 0 { nodes.get(idx - 1) } else { None };
+        let current = &nodes[idx];
+        let after = nodes.get(idx + 1);
+
+        let mut context = PrintingContext {
+            previous_node: previous,
+            after_node: after,
+            parent_nodes: context.parent_nodes.clone(),
+            indentation: context.indentation,
+        };
+
+        print_node(writer, current, &mut context).await;
     }
 }
 
-async fn print_tag(writer: &mut BufWriter<File>, tag: &HtmlTag, indentation: u16) {
-    print_indentation(writer, indentation).await;
+async fn print_tag(writer: &mut BufWriter<File>, tag: &HtmlTag, context: &PrintingContext<'_>) {
+    print_indentation(writer, context).await;
 
     writer.write_all(b"<").await.unwrap();
     writer.write_all(tag.name.as_bytes()).await.unwrap();
@@ -71,7 +120,7 @@ async fn print_tag(writer: &mut BufWriter<File>, tag: &HtmlTag, indentation: u16
     for (key, value) in &tag.attributes {
         if tag.attributes.len() > 2 || tag.name.len() > 24 {
             writer.write_all(b"\n").await.unwrap();
-            print_indentation(writer, indentation + 2).await;
+            print_indentation(writer, &context.increase_indentation_by(2)).await;
         } else {
             writer.write_all(b" ").await.unwrap();
         }
@@ -93,10 +142,10 @@ async fn print_tag(writer: &mut BufWriter<File>, tag: &HtmlTag, indentation: u16
         writer.write_all(b">").await.unwrap();
     }
 
-    print_node_list(writer, &tag.children, indentation + 1).await;
+    print_node_list(writer, &tag.children, &context.increase_indentation_by(1)).await;
 
     if !tag.children.is_empty() {
-        print_indentation(writer, indentation).await;
+        print_indentation(writer, context).await;
     }
 
     if !tag.self_closed {
@@ -106,41 +155,60 @@ async fn print_tag(writer: &mut BufWriter<File>, tag: &HtmlTag, indentation: u16
     }
 }
 
-async fn print_plain(writer: &mut BufWriter<File>, plain: &HtmlPlain, indentation: u16) {
-    print_indentation(writer, indentation).await;
+async fn print_plain(
+    writer: &mut BufWriter<File>,
+    plain: &HtmlPlain,
+    context: &PrintingContext<'_>,
+) {
+    print_indentation(writer, context).await;
     writer.write_all(plain.plain.as_bytes()).await.unwrap();
 }
 
-async fn print_vue_block(writer: &mut BufWriter<File>, vue: &VueBlock, indentation: u16) {
-    print_indentation(writer, indentation).await;
+async fn print_vue_block(
+    writer: &mut BufWriter<File>,
+    vue: &VueBlock,
+    context: &PrintingContext<'_>,
+) {
+    print_indentation(writer, context).await;
     writer.write_all(b"{{ ").await.unwrap();
     writer.write_all(vue.content.as_bytes()).await.unwrap();
     writer.write_all(b" }}").await.unwrap();
 }
 
-async fn print_twig_block(writer: &mut BufWriter<File>, twig: &TwigBlock, indentation: u16) {
-    print_indentation(writer, indentation).await;
+async fn print_twig_block(
+    writer: &mut BufWriter<File>,
+    twig: &TwigBlock,
+    context: &PrintingContext<'_>,
+) {
+    print_indentation(writer, context).await;
     writer.write_all(b"{% block ").await.unwrap();
     writer.write_all(twig.name.as_bytes()).await.unwrap();
     writer.write_all(b" %}").await.unwrap();
 
-    print_node_list(writer, &twig.children, indentation + 1).await;
+    print_node_list(writer, &twig.children, &context.increase_indentation_by(1)).await;
 
-    print_indentation(writer, indentation).await;
+    print_indentation(writer, context).await;
     writer.write_all(b"{% endblock %}").await.unwrap();
 }
 
-async fn print_twig_parent_call(writer: &mut BufWriter<File>, indentation: u16) {
-    print_indentation(writer, indentation).await;
+async fn print_twig_parent_call(writer: &mut BufWriter<File>, context: &PrintingContext<'_>) {
+    print_indentation(writer, context).await;
     writer.write_all(b"{% parent %}").await.unwrap();
 }
 
-async fn print_whitespace(writer: &mut BufWriter<File>) {
+async fn print_whitespace(writer: &mut BufWriter<File>, context: &PrintingContext<'_>) {
+    if let Some(prev) = context.previous_node {
+        if let HtmlNode::TwigBlock(_) = prev {
+            // print another whitespace.
+            writer.write_all(b"\r\n").await.unwrap();
+        }
+    }
+
     writer.write_all(b"\r\n").await.unwrap();
 }
 
-async fn print_indentation(writer: &mut BufWriter<File>, indentation: u16) {
-    for _ in 0..indentation {
+async fn print_indentation(writer: &mut BufWriter<File>, context: &PrintingContext<'_>) {
+    for _ in 0..context.indentation {
         writer.write_all(b"    ").await.unwrap();
     }
 }
