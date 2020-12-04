91ce83994e0778e30691cb6028386cbce6a85d26
Malte Janz <malte.janz@yahoo.de>
2020-12-04 14:38:51 +0100

    Added more micro benchmarks

--- libs/twig/benches/parsing_benchmark.rs
+++ libs/twig/benches/parsing_benchmark.rs
@@ -11,5 +11,21 @@ fn parsing_benchmark(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, parsing_benchmark);
+fn failing_parsing_benchmark(c: &mut Criterion) {
+    let input = fs::read_to_string("./fixtures/complex-failing.html.twig")
+        .expect("can't find fixtures/complex-failing.html.twig in project folder");
+
+    c.bench_function("parsing complex-failing.html.twig", |b| {
+        b.iter(|| parse(black_box(&input)))
+    });
+
+    let err = parse(&input)
+        .expect_err("expected failed parsing for fixtures/complex-failing.html.twig in project folder");
+
+    c.bench_function("generating human error from failed parsing", |b| {
+        b.iter(|| err.pretty_helpful_error_string(black_box(&input)))
+    });
+}
+
+criterion_group!(benches, parsing_benchmark, failing_parsing_benchmark);
 criterion_main!(benches);
