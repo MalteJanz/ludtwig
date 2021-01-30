19af3c0221cdec4116d34fa58215ed4d09aea513
Malte Janz <malte.janz@yahoo.de>
2021-01-30 20:25:55 +0100

    Replaced example analyzer with one that checks for duplicate twig block names

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -1,26 +1,109 @@
 use crate::output::Output;
 use crate::process::FileContext;
+use std::collections::HashSet;
 use std::future::Future;
 use std::pin::Pin;
 use std::sync::Arc;
-use twig::ast::{SyntaxNode, TwigStructure};
+use twig::ast::{SyntaxNode, TwigBlock, TwigStructure};
 
 /// Entry function for doing the analyzing.
 /// It spawns all analyzer functions concurrently.
 pub async fn analyze<'a>(file_context: Arc<FileContext>) {
     let clone = Arc::clone(&file_context);
+
+    tokio::spawn(async move {
+        let mut block_names = HashSet::new();
+        analyze_blocks(&file_context.tree, &clone, &mut block_names).await;
+    })
+    .await
+    .unwrap();
+
+    /*
     tokio::spawn(async move {
         analyze_blocks(&file_context.tree, &clone, None).await;
     })
     .await
     .unwrap();
+    */
 
     // run more analyzers in parallel...
 }
 
-/// Example analyzer:
-/// Checks if the twig block name contains the block name of it's parent twig block.
-/// This should reduce typing errors that humans will produce.
+fn analyze_blocks<'a>(
+    node: &'a SyntaxNode,
+    file_context: &'a FileContext,
+    block_names: &'a mut HashSet<String>,
+) -> Pin<Box<dyn Future<Output = ()> + 'a + Send>> {
+    Box::pin(async move {
+        match node {
+            SyntaxNode::Root(root) => {
+                for child in root {
+                    analyze_blocks(child, file_context, block_names).await;
+                }
+            }
+            SyntaxNode::Tag(tag) => {
+                // TODO: blocks in html tag attributes are not visited yet. Iteration logic should be refactored.
+                for child in &tag.children {
+                    analyze_blocks(child, file_context, block_names).await;
+                }
+            }
+            SyntaxNode::TwigStructure(twig_structure) => match twig_structure {
+                TwigStructure::TwigBlock(twig) => {
+                    check_twig_block_name_for_duplicate(twig, file_context, block_names).await;
+
+                    for child in &twig.children {
+                        analyze_blocks(child, file_context, block_names).await;
+                    }
+                }
+                TwigStructure::TwigFor(twig) => {
+                    for child in &twig.children {
+                        analyze_blocks(child, file_context, block_names).await;
+                    }
+                }
+                TwigStructure::TwigIf(twig) => {
+                    for arm in &twig.if_arms {
+                        for child in &arm.children {
+                            analyze_blocks(child, file_context, block_names).await;
+                        }
+                    }
+                }
+                TwigStructure::TwigApply(twig) => {
+                    for child in &twig.children {
+                        analyze_blocks(child, file_context, block_names).await;
+                    }
+                }
+                TwigStructure::TwigSetCapture(twig) => {
+                    for child in &twig.children {
+                        analyze_blocks(child, file_context, block_names).await;
+                    }
+                }
+            },
+            _ => {}
+        }
+    })
+}
+
+async fn check_twig_block_name_for_duplicate(
+    block: &TwigBlock<SyntaxNode>,
+    file_context: &FileContext,
+    block_names: &mut HashSet<String>,
+) {
+    if block_names.contains(&block.name) {
+        file_context
+            .send_output(Output::Warning(format!(
+                "Duplicate twig block name found: '{}'",
+                &block.name
+            )))
+            .await;
+    } else {
+        block_names.insert(block.name.clone());
+    }
+}
+
+/*
+// Example analyzer:
+// Checks if the twig block name contains the block name of it's parent twig block.
+// This should reduce typing errors that humans will produce.
 fn analyze_blocks<'a>(
     node: &'a SyntaxNode,
     file_context: &'a FileContext,
@@ -58,3 +141,4 @@ fn analyze_blocks<'a>(
         }
     })
 }
+*/
