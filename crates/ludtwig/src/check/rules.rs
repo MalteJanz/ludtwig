22fc5a924ba05f124bf56de0854da0553c93fd60
Lars Kemper <l.kemper@shopware.com>
2023-07-21 11:05:56 +0200

    Added rule twig-logic-same-as

--- crates/ludtwig/src/check/rules.rs
+++ crates/ludtwig/src/check/rules.rs
@@ -9,6 +9,7 @@ use crate::check::rules::twig_block_name_snake_case::RuleTwigBlockNameSnakeCase;
 use crate::check::rules::twig_hash_key_no_quotes::RuleTwigHashKeyNoQuotes;
 use crate::check::rules::twig_logic_and::RuleTwigLogicAnd;
 use crate::check::rules::twig_logic_or::RuleTwigLogicOr;
+use crate::check::rules::twig_logic_same_as::RuleTwigLogicSameAs;
 use crate::check::rules::twig_string_quotation::RuleTwigStringQuotation;
 use crate::check::rules::twig_use_is_not_same_as::RuleTwigUseIsNotSameAs;
 use crate::check::rules::unknown_token::RuleUnknownToken;
@@ -28,6 +29,7 @@ mod twig_block_name_snake_case;
 mod twig_hash_key_no_quotes;
 mod twig_logic_and;
 mod twig_logic_or;
+mod twig_logic_same_as;
 mod twig_string_quotation;
 mod twig_use_is_not_same_as;
 mod unknown_token;
@@ -45,6 +47,7 @@ pub static RULE_DEFINITIONS: &[&'static dyn Rule] = &[
     &RuleHtmlAttributeNameKebabCase,
     &RuleTwigLogicAnd,
     &RuleTwigLogicOr,
+    &RuleTwigLogicSameAs,
     &RuleTwigStringQuotation,
     &RuleHtmlStringQuotation,
     &RuleTwigHashKeyNoQuotes,
